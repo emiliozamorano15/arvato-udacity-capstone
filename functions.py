d14bccdff0a0fbd35867d5a89a58bab44ad4db37
Emilio Zamorano <emilio@zamoranodeacha.com>
2021-07-03 09:41:38 +0200

    feat: added functions module

---
+++ functions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+def clean_data(df, drop = False):
+    '''
+    Function to clean Arvato's datasets. It mainly changes data format for certain columns,
+    and drops columns (rows) which exceed a given threshold of missing values.
+
+    INPUT
+    df: pandas dataframe (from Arvato's )
+    drop: True/False optionally switch on/off dropping columns and rows with empty values
+
+    OUTPUT
+    clean_df: pandas dataframe
+    '''
+
+
+if __name__ == '__main__':
+    pass
