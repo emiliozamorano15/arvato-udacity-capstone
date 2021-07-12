c04eed5c7581a4d914a3e5ae142c37d5c0f932a9
Emilio Zamorano <emilio@zamoranodeacha.com>
2021-07-12 22:37:42 +0200

    start supervised model

--- functions.py
+++ functions.py
@@ -157,27 +157,30 @@ def get_cluster_centers(cluster_pipeline, num_cols, col_names):
  
     return df
 
-def model_performance(X, y, model, num_samples):
+def model_performance(X, y, model):
     """
-    Draw learning curve that shows the validation and training auc_score of an estimator 
-    for varying numbers of training samples.
+    Run a series of cross_validation/training splits and plot the 
+    resulting AUC score
     
     INPUT:
         X: Predictors Matrix
         y: Target Vetor
         estimator: sklearn predictor object with fit and predict methods
-        num_samples: number of training samples to plot
-        
     """
     from sklearn.model_selection import learning_curve
     import matplotlib.pyplot as plt
+    
+    num_samples = 5
 
     train_sizes, train_scores, test_scores = learning_curve(
         model
-        , X
-        , y
+        , X.iloc[X.sample(frac=1, random_state = 42).index]
+        , y.iloc[X.sample(frac=1, random_state = 42).index]
+        , cv = 3
         , scoring = 'roc_auc'
-        , train_sizes=np.linspace(.1, 1.0, num_samples)
+        , train_sizes=np.linspace(0.1, 1.0, num_samples)
+        , verbose = 4
+        , n_jobs= -1
         )
 
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -186,9 +189,9 @@ def model_performance(X, y, model, num_samples):
     print("AUC cv = {}".format(cv_scores_mean[-1].round(2)))
 
 
-    plt.plot(np.linspace(.1, 1.0, num_samples)*100, train_scores_mean,
+    plt.plot(np.linspace(0.1, 1.0, num_samples)*100, train_scores_mean,
              label="Training score")
-    plt.plot(np.linspace(.1, 1.0, num_samples)*100, cv_scores_mean,
+    plt.plot(np.linspace(0.1, 1.0, num_samples)*100, cv_scores_mean,
              label="Cross-validation score")
 
     plt.title("Learning Curves")
@@ -199,6 +202,126 @@ def model_performance(X, y, model, num_samples):
     print("")
     plt.show()
 
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5),
+                        verbose=0):
+    """
+    Generate 3 plots: the test and training learning curve, the training
+    samples vs fit times curve, the fit times vs score curve.
+    Source: [https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html]
+    Parameters
+    ----------
+    estimator : estimator instance
+        An estimator instance implementing `fit` and `predict` methods which
+        will be cloned for each validation.
+
+    title : str
+        Title for the chart.
+
+    X : array-like of shape (n_samples, n_features)
+        Training vector, where ``n_samples`` is the number of samples and
+        ``n_features`` is the number of features.
+
+    y : array-like of shape (n_samples) or (n_samples, n_features)
+        Target relative to ``X`` for classification or regression;
+        None for unsupervised learning.
+
+    axes : array-like of shape (3,), default=None
+        Axes to use for plotting the curves.
+
+    ylim : tuple of shape (2,), default=None
+        Defines minimum and maximum y-values plotted, e.g. (ymin, ymax).
+
+    cv : int, cross-validation generator or an iterable, default=None
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+
+          - None, to use the default 5-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, default=None
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    train_sizes : array-like of shape (n_ticks,)
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the ``dtype`` is float, it is regarded
+        as a fraction of the maximum size of the training set (that is
+        determined by the selected validation method), i.e. it has to be within
+        (0, 1]. Otherwise it is interpreted as absolute sizes of the training
+        sets. Note that for classification the number of samples usually have
+        to be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn.model_selection import learning_curve
+
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True,
+                       verbose=verbose)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
 
 if __name__ == '__main__':
     pass
